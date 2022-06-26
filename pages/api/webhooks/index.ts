1c191d330443979466555b5470ab2f2414629306
Nestor Mamani <nmamanipantoja@gmail.com>
2022-06-26 09:14:33 -0500

    💥 Hotfix

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -50,27 +50,22 @@ const handleWhatsappWebhook = async (
   }
 
   if (req.method === 'GET') {
-    try {
-      const verify_token = process.env.VERIFY_TOKEN;
-      let mode = req.query['hub.mode'];
-      let token = req.query['hub.verify_token'];
-      let challenge = req.query['hub.challenge'];
+    const verify_token = process.env.VERIFY_TOKEN;
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-      // Check if a token and mode were sent
-      if (mode && token) {
-        // Check the mode and token sent are correct
-        if (mode === 'subscribe' && token === verify_token) {
-          // Respond with 200 OK and challenge token from the request
-          console.log('WEBHOOK_VERIFIED');
-          res.status(200).send(challenge);
-        } else {
-          // Responds with '403 Forbidden' if verify tokens do not match
-          res.status(403);
-        }
+    // Check if a token and mode were sent
+    if (mode && token) {
+      // Check the mode and token sent are correct
+      if (mode === 'subscribe' && token === verify_token) {
+        // Respond with 200 OK and challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.status(403);
       }
-      res.status(200).json(req.body);
-    } catch (error) {
-      res.status(500).json({ error });
     }
   }
 };
