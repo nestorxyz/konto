7c280368821c0786f53277e2544a1d6996c2e23e
Nestor Mamani <nmamanipantoja@gmail.com>
2022-06-29 21:19:15 -0500

    💄 Add phone UI

--- src/screens/dashboard/onboarding/AddPhone.tsx
+++ src/screens/dashboard/onboarding/AddPhone.tsx
@@ -1,20 +1,64 @@
 // Libraries
-import { Input } from '@nextui-org/react';
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Input, Button, Loading } from '@nextui-org/react';
 
 // Components
 import OnboardingContainer from './OnboardingContainer';
 
 const AddPhoneOnboarding: React.FC<{}> = () => {
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      phone: '',
+    },
+    validationSchema: Yup.object({
+      phone: Yup.string().required('El teléfono es requerido'),
+    }),
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   const AddPhone: React.FC = () => {
     return (
       <>
-        <h2 className="text-gray-800 mx-auto mt-4">Agrega tu número</h2>
+        <h2 className="text-gray-800 mx-auto mb-10">Agrega tu número</h2>
+        <form onSubmit={formik.handleSubmit} className="w-full flex flex-col">
+          <Input
+            underlined
+            placeholder="Ej. 989009435"
+            color="primary"
+            size="xl"
+            type="tel"
+            className="mb-6"
+            name="phone"
+            aria-label="Número de teléfono"
+            value={formik.values.phone}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            status={
+              formik.errors.phone && formik.touched.phone ? 'error' : 'default'
+            }
+            helperColor="error"
+            helperText={
+              formik.errors.phone && formik.touched.phone
+                ? formik.errors.phone
+                : ''
+            }
+          />
+          <Button size="lg" type="submit" disabled={!formik.isValid || loading}>
+            {loading ? <Loading size="sm" color="primary" /> : 'Agregar número'}
+          </Button>
+        </form>
       </>
     );
   };
 
   return (
-    <OnboardingContainer>
+    <OnboardingContainer className="min-w-[320px] md:w-lg mt-8 mx-auto">
       <AddPhone />
     </OnboardingContainer>
   );
--- src/screens/dashboard/onboarding/OnboardingContainer.tsx
+++ src/screens/dashboard/onboarding/OnboardingContainer.tsx
@@ -1,17 +1,24 @@
+// Helpers
+import classNames from 'lib/classNames';
+
 interface IOnboardingContainerProps {
   children: React.ReactNode;
+  className?: string;
 }
 
 const OnboardingContainer: React.FC<IOnboardingContainerProps> = ({
   children,
+  className,
 }) => {
   return (
-    <div className="flex p-6 flex-col lg:mx-12 xl:mx-24">
+    <div className="flex p-6 flex-col lg:mx-12 xl:mx-24 min-h-screen">
       <div className="flex items-center">
         <img src="/logo.svg" width="58" height="61.5" />
         <p className="font-bold text-4xl text-primary ml-4">Konto</p>
       </div>
-      {children}
+      <div className={classNames(className, 'flex-1 flex flex-col')}>
+        {children}
+      </div>
     </div>
   );
 };
